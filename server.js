0090510b43c1b33f5540b2031f589bfc6a8d6383
utsav <utsavnagaria19@gmail.com>
2021-06-08 23:47:02 +0530

    e-shop

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
+const path = require('path')
 require("dotenv").config();
 
 // app
@@ -31,12 +32,12 @@ readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 // port
 const port = process.env.PORT || 8000;
 
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static('client/build'))
-  const path = require('path')
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(_dirname,'client','build','index.html'))
+
+  app.use(express.static(path.join(__dirname, "build")))
+  
+  app.get("/*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'build','index.html'))
   })
-}
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
